dc3989188175ed4076a689a05601324eebd0b8ec
miaravnanger <mia_maroen@hotmail.com>
2025-10-20 16:05:51 +0200

    sette alt sammen

--- app.js
+++ app.js
@@ -1,77 +1,140 @@
-// //Movie constructor
-// class Movie {
-//   constructor(id, title, genre, year, rating = 0, favorite = false )
-//   {
-//   this.id = id;
-//   this.title = title;
-//   this.genre = genre;
-//   this.year = year;
-//   this.rating = rating;
-//   this.favorite = favorite;
+//Movie constructor
+class Movie {
+  constructor(id, title, genre, year, rating = 0, favorite = false) {
+    this.id = id;
+    this.title = title;
+    this.genre = genre;
+    this.year = year;
+    this.rating = rating;
+    this.favorite = favorite;
+  }
+}
+
+// Ui Constructor
+class UI {
+  //lagre til local storage
+  static getMovies() {
+    return JSON.parse(localStorage.getItem("movies")) || [];
+  }
+  static saveMovies(movies) {
+    localStorage.setItem("movies", JSON.stringify(movies));
+  }
+  //legg til movie i listen
+  static addMovieToList(movie) {
+  const list = document.querySelector("#movie-list");
+  const {id, title, genre, year, rating } = movie;
+      //lag tr element
+  const row = document.createElement("tr");
+  row.dataset.id = id;
+    //legg inn colonner
+    row.innerHTML = `
+  <td>${movie.title}</td>
+  <td>${movie.genre}</td>
+  <td>${movie.year}</td>
+  <td class="rating">${UI.renderStars(rating)}</td>
+    <td><a href="#" class="delete">X<a/></td>
+  `;
+    list.appendChild(row);
+  }
+  static renderStars(rating){
+    let html ="";
+    for (let i = 1; i <= 5; i++){
+      html += `<i class="fa-solid fa-star ${i <= rating ? "active": ""}" data-value="${i}"></i>`;
+    }
+    return html;
+  }
+  static deleteMovie(id) {
+    const movies = UI.getMovies().filter(m => m.id !== id);
+    UI.saveMovies(movies);
+    UI.displayMovies();
+  }
+
+  static updateRating(id, newRating){
+    const movies = UI.getMovies().map(movie => movie.id === id ? {...movie, rating: newRating} : movie);
+    UI.saveMovies(movies);
+    UI.displayMovies();
+  }
+static displayMovies() {
+  const list = document.querySelector("#movie-list");
+  list.innerHTML = "";
+  const movies = UI.getMovies();
+  movies.map(movie => UI.addMovieToList(movie));
+}
+  static showAlert(message, className) {
+    //lag div, legg til klasse og tekst
+    const div = document.createElement("div");
+    div.className = `alert ${className}`;
+    div.textContent = message;
+    //hent containeren og form
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#movie-form");
+    // sett inn alert
+    container.insertBefore(div, form);
+    // Timeout etter 3 sek
+    setTimeout(() =>
+     div.remove(), 3000);
+    };
+    static clearFields() {
+    //clear ut feltene
+    document.getElementById("title").value = "";
+    document.getElementById("genre").value = "";
+    document.getElementById("year").value = "";
+  }
+}
+
+//legg til film 
+// targeter og legger til event listener for å legge til en film
+document.querySelector("#movie-form").addEventListener("submit", e => {
+  e.preventDefault();
+    //henter form valuene
+  const title = document.querySelector("#title").value.trim();
+  const genre = document.querySelector("#genre").value.trim();
+  const year = document.querySelector("#year").value.trim();
+    // instantere UI
+  // const ui = new UI();
+  // validere
+  if (!title|| !genre|| !year) {
+    // error alert
+     UI.showAlert("Please fill inn all fields", "error");
+     return;
+  }
+    // instanrere movie
+    const movie = new Movie(Date.now().toString(), title, genre, year);
+    const movies = UI.getMovies();
+    movies.push(movie);
+    UI.saveMovies(movies);
+    UI.displayMovies();
+    UI.showAlert("Movie added to list!", "success");
+    UI.clearFields();
+});
+
+  // Slett film
+  // Event listener for delete
+document.querySelector("#movie-list").addEventListener("click", e => {
+    if (e.target.classList.contains ("delete")) {
+      const id = e.target.closest("tr").dataset.id;
+      UI.deleteMovie(id);
+      UI.showAlert("Movie deleted", "success");
+          e.preventDefault();
+    }
+
+  //Klikk på stjernene
+  if (e.target.classList.contains("fa-star")){
+
+    const id = e.target.closest("tr").dataset.id;
+    const newRating = parseInt(e.target.dataset.value);
+    UI.updateRating(id, newRating);
+  }
+  });
+
+//   clearFields() {
+//     //clear ut feltene
+//     document.getElementById("title").value = " ";
+//     document.getElementById("genre").value = " ";
+//     document.getElementById("year").value = " ";
+//   }
 // }
 
-// // Ui Constructor
-// //   class UI {
-// //   addMovieToList(){
-
-// // }
-// //  showAlert(){
-
-// // }
-// //  deleteMovie(){
-
-// // }
-// //  editMovie(){
-
-// // }
-// //  clearFields(){
-
-// // }
-// //   }
-
-// //legg Movie til listen
-// UI.prototype.addMovieToList = function (movie) {
-//   const list = document.querySelector("#movie-list");
-
-//   //lag tr element
-//   const row = document.createElement("tr");
-//   //legg inn colonner
-//   row.innerHTML = `
-//   <td>${movie.title}</td>
-//   <td>${movie.genre}</td>
-//   <td>${movie.year}</td>
-//     <td><a href="#" class="delete">X</td>
-//   `;
-//   list.appendChild(row);
-// };
-
-// //clear ut feltene
-// UI.prototype.clearFields = function (){
-//   document.querySelector('#title').value = ' ';
-//    document.querySelector("#genre").value = ' ';
-//     document.querySelector("#year").value = ' ';
-// }
-// // event listeners
-
-// document.querySelector("#movie-form").addEventListener("submit", (e) => {
-//   //henter form valuene
-//   const title = document.querySelector("#title").value,
-//     genre = document.querySelector("#genre").value,
-//     year = document.querySelector("#year").value;
-
-//   // instanrere movie
-//   const movie = new Movie(title, genre, year);
-
-//   // instantere UI
-//   const ui = new UI();
-
-//   //legg movie til listen
-//   ui.addMovieToList(movie);
-
-//   //clear ut feltene
-//   ui.clearFields();
-//   e.preventDefault();
-// });
 
 // hent stjernene fra html for å manipulere de
 let rating = 0;
