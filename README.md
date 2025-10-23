cb79d66f297f5f9946cde2bbc110ec9752dfd88f
miaravnanger <mia_maroen@hotmail.com>
2025-10-23 22:28:49 +0200

    rydding og readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Filmliste
+
+Et filmregister laget med HTML, CSS og JavaScript, som lar brukeren legge til, redigere, filtrere og sortere filmer, og lagrer alt i local storage.
+
+# Funksjoner
+
+- Legg til film (tittel, sjanger, år og rating)
+- Marker filmer som favoritt
+- Rediger eller slett eksisterende filmer fra listen
+- Tøm hele listen
+- Filtrer og sorter listen etter eget input, eller årstall, rating, favoritter, sist lagt til eller alfabetisk rekkefølge(tittel og sjanger)
+- Automatisk lagring i localStorage
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Movie constructor
+// Movie-klasse
 class Movie {
   constructor(id, title, genre, year, rating = 0, favorite = false) {
     this.id = id;
@@ -10,23 +10,25 @@ class Movie {
   }
 }
 
-// Ui Constructor
+// UI-klasse
 class UI {
-  //lagre til local storage
+  // Hent filmer fra localStorage
   static getMovies() {
     return JSON.parse(localStorage.getItem("movies")) || [];
   }
+  // Lagre filmer i localStorage
   static saveMovies(movies) {
     localStorage.setItem("movies", JSON.stringify(movies));
   }
-  //legg til movie i listen
+  // Legg til en ny film i listen
   static addMovieToList(movie) {
     const list = document.querySelector("#movie-list");
     const { id, title, genre, year, rating } = movie;
-    //lag tr element
+    // Ny rad i HTML-tabellen for film-listen
     const row = document.createElement("tr");
+    // Henter ut data-verdien til elementet og lagrer det som "id" for å bruke seinere når vi skal slette og redigere filmer
     row.dataset.id = id;
-    //legg inn colonner
+    // Legg inn colonner for tittel, sjanger, utgitt, rating, og linker til å lagre som favoritt, redigere og slette filmer
     row.innerHTML = `
   <td>${movie.title}</td>
   <td>${movie.genre}</td>
@@ -40,6 +42,8 @@ class UI {
   `;
     list.appendChild(row);
   }
+  // Viser stjerne-vurderingen på listen (readOnly så de ikke lengre kan trykkes på)
+  // Trenger bare indexen, så bruker _ for å hoppe over elementet
   static renderStars(rating) {
     return Array.from(
       { length: 5 },
@@ -50,13 +54,14 @@ class UI {
     ).join("");
   }
 
-  //slett filme fra den synlige listen
+  // Slette film
   static deleteMovie(id) {
     const movies = UI.getMovies().filter((m) => m.id !== id);
     UI.saveMovies(movies);
     UI.displayMovies();
   }
 
+  // Oppdatere rating
   static updateRating(id, newRating) {
     const movies = UI.getMovies().map((movie) =>
       movie.id === id ? { ...movie, rating: newRating } : movie
@@ -65,7 +70,7 @@ class UI {
     UI.displayMovies();
   }
 
-  //vis filmene på siden
+  // Vise alle filmer i tabellen
   static displayMovies() {
     const list = document.querySelector("#movie-list");
     const clearBtn = document.querySelector(".clearAll");
@@ -76,52 +81,53 @@ class UI {
     clearBtn.style.display = movies.length ? "inline-block" : "none";
   }
 
-  // Lager en alert som kan brukes igjen
+  // Lager en div med en alert som kan brukes igjen for suksess og feilmeldinger
   static showAlert(message, className) {
-    //lag div, legg til klasse og tekst
     const div = document.createElement("div");
     div.className = `alert ${className}`;
     div.textContent = message;
-    //hent containeren og form
+    // Hent containeren og film-skjemaet
     const container = document.querySelector(".container");
     const form = document.querySelector("#movie-form");
-    // sett inn alert
+    // Sett inn alert-diven over skjemaet
     container.insertBefore(div, form);
-    // Timeout etter 3 sek
+    // Timeout så den forsvinner etter 1,5sek
     setTimeout(() => div.remove(), 1500);
   }
+  // Nullstill inputfeltene og stjerne-rangeringen
   static clearFields() {
-    //clear ut feltene
     document.getElementById("title").value = "";
     document.getElementById("genre").value = "";
     document.getElementById("year").value = "";
 
-    //nullstill stjernene
     const stars = document.querySelectorAll("#rating-stars i");
     stars.forEach((star) => star.classList.remove("active"));
     rating = 0;
   }
 }
+//
+//                    Event listeners
+//
+// Gjør sånn at innholdet i film-listen vises når man åpner siden
 document.addEventListener("DOMContentLoaded", UI.displayMovies);
-//legg til film
-// targeter og legger til event listener for å legge til en film
+
+// Legg til film
+// Lytt etter submit
 document.querySelector("#movie-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  //henter form valuene
+  // Henter skjema-verdiene
   const title = document.querySelector("#title").value.trim();
   const genre = document.querySelector("#genre").value.trim();
   const year = document.querySelector("#year").value.trim();
-  // instantere UI
-  // const ui = new UI();
-  // validere
+
   if (!title || !genre || !year) {
-    // error alert
+    // Error alert hvis noen av feltene ikke er fylt ut
     UI.showAlert("Vennligst fyll inn alle felt", "error");
     return;
   }
 
-  //edit knapp
-
+  // Edit knapp
+  // Gir filmen som blir targeted av edit knappen, en editing-id mens den redigeres, og fjerner den når den er ferdig redigert. Hvis filmen ikke har edit id, så legg til en ny film.
   const editingId = e.target.dataset.editing;
   const movies = UI.getMovies();
   if (editingId) {
@@ -148,13 +154,17 @@ document.querySelector("#movie-form").addEventListener("submit", (e) => {
   UI.clearFields();
 });
 
+// Click events i film-listen
 document.querySelector("#movie-list").addEventListener("click", (e) => {
+  // Slett film
+  // Hvis elementet som blir klikket på har klassen "delete", finn den nærmeste raden sin id og slett denne filmen fra UI.
   if (e.target.classList.contains("delete")) {
     const id = e.target.closest("tr").dataset.id;
     UI.deleteMovie(id);
     UI.showAlert("Film fjernet!", "success");
     e.preventDefault();
   }
+  // Marker som favoritt og lag nytt array med oppdatert verdi hvis filmen ble markert som favoritt
   if (e.target.classList.contains("fa-heart")) {
     const id = e.target.closest("tr").dataset.id;
     const movies = UI.getMovies();
@@ -164,25 +174,18 @@ document.querySelector("#movie-list").addEventListener("click", (e) => {
     UI.saveMovies(updatedMovies);
     UI.displayMovies();
   }
-  //Klikk på stjernene
-  if (e.target.classList.contains("fa-star")) {
-    const id = e.target.closest("tr").dataset.id;
-    const newRating = parseInt(e.target.dataset.value);
-    UI.updateRating(id, newRating);
-  }
 
   // Rediger film
   if (e.target.classList.contains("fa-pencil")) {
     const id = e.target.closest("tr").dataset.id;
     const movies = UI.getMovies();
+    // Finn filmen som har samme id som filmen som ble trykket på
     const movieToEdit = movies.find((m) => m.id === id);
-
-    //fyll in feltene i edit mode
+    // Fyll in feltene i edit mode
     document.getElementById("title").value = movieToEdit.title;
     document.getElementById("genre").value = movieToEdit.genre;
     document.getElementById("year").value = movieToEdit.year;
-
-    //marker stjernene på nytt
+    // Marker stjernene på nytt
     const stars = document.querySelectorAll("#rating-stars i");
     rating = movieToEdit.rating;
     stars.forEach((star, i) => {
@@ -190,51 +193,42 @@ document.querySelector("#movie-list").addEventListener("click", (e) => {
         ? star.classList.add("active")
         : star.classList.remove("active");
     });
-    //lagre id på filmen som ble redigert
+    // Vis alert når man trykker på rediger
     document.querySelector("#movie-form").dataset.editing = id;
     UI.showAlert("Rediger og send inn på nytt", "success");
   }
 });
 
-//   clearFields() {
-//     //clear ut feltene
-//     document.getElementById("title").value = " ";
-//     document.getElementById("genre").value = " ";
-//     document.getElementById("year").value = " ";
-//   }
-// }
-
-// hent stjernene fra html for å manipulere de
+// Rating-stjernene i skjemaet, la bruker velge rating ved å klikke
 let rating = 0;
 const stars = document.querySelectorAll("#rating-stars i");
 
-// loop gjennom stars nodeslisten
+// Loop gjennom stars
 stars.forEach((star, index1) => {
-  //legg til eventlistener for klikk
+  // Legg til eventlistener for klikk, lagre index1 + 1 som "rating" og loop gjennom på nytt
   star.addEventListener("click", () => {
     rating = index1 + 1;
-    // loop gjennom på nytt
     stars.forEach((star, index2) => {
-      // legg til "active" klasse på stjernen som er klikket på og stjerner med lavere index, og fjern "active" klassen fra stjerner med høyre index
-      index1 >= index2
+      index1 >= index2 // Hvis index 1 er mindre enn eller lik index 2, legg til "active" klasse, hvis ikke, fjern "active" klasse
         ? star.classList.add("active")
         : star.classList.remove("active");
     });
   });
 });
 
+// Tøm hele listen
 document.querySelector(".clearAll").addEventListener("click", () => {
   if (confirm("Er du sikker på at du vil tømme hele listen?")) {
-    localStorage.removeItem("movies"); //fjerner alt fra local storage
-    UI.displayMovies(); //oppdaterer listeinnholdet til tomt
+    localStorage.removeItem("movies"); // Fjerner alt fra local storage
+    UI.displayMovies(); //Oppdaterer listeinnholdet til tomt
     UI.showAlert("Listen er tømt!", "success");
   }
 });
 
-// filtrer etter brukerens input
 document.querySelector("#filter").addEventListener("input", (e) => {
   const query = e.target.value.toLowerCase();
   const movies = UI.getMovies();
+  // Filtrer basert på om tittel, sjanger eller årstall inneholder det brukeren skriver inn
   const filtered = movies.filter(
     (m) =>
       m.title.toLowerCase().includes(query) ||
@@ -243,18 +237,20 @@ document.querySelector("#filter").addEventListener("input", (e) => {
   );
   UI.renderFilteredMovies(filtered);
 });
+// Vis ny liste basert på filtreringen
 UI.renderFilteredMovies = function (movies) {
   const list = document.querySelector("#movie-list");
   list.innerHTML = "";
   movies.forEach((movie) => UI.addMovieToList(movie));
 };
 
-// sorter
-// Event listener som lytter på når brukeren forandrer/velger ny verdi fra sorteringslisten
+// Sortering
+// Event listener som kjører når brukeren forandrer/velger ny verdi fra sorteringslisten
 document.querySelector("#sort").addEventListener("change", (e) => {
+  // Lagre den valgte verdien som "value"
   const value = e.target.value;
   const movies = UI.getMovies();
-
+  // Sorter listen basert på value, hvis ingenting er valgt så behold listen som den er
   if (value === "rating") {
     movies.sort((a, b) => b.rating - a.rating);
   } else if (value === "rating2") {
